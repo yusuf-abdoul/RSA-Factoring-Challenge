1f6fb76911da57e3c3c0454767458283631af1bf
Yusuf Abdul-hayyi <abdulhayyiyusuf2015@gmail.com>
2023-01-29 18:43:30 -0800

    task done

--- factor.c
+++ factor.c
@@ -1,33 +1,86 @@
-#include "factor.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <gmp.h>
 
-/**
- * main - main function
- *
- *
- * Return: void
- */
-int main(int argc, char *argv[])
+void factor(mpz_t *p, mpz_t n)
 {
-	FILE *fptr;
-	size_t count;
-	ssize_t line;
-	char *buffer = NULL;
+	mpz_t r;
 
+	mpz_set_ui(*p, 2U);
+	if (mpz_divisible_p(n, *p))
+		return;
 
-	if (argc != 2)
+	mpz_init(r);
+	mpz_sqrt(r, n);
+
+	if (mpz_divisible_p(n, r))
 	{
-		fprintf(stderr, "Usage: factor <filename>\n");
+		mpz_set(*p, r);
+		mpz_clear(r);
+		return;
+	}
+
+	mpz_set_ui(*p, 3U);
+
+	while (!mpz_divisible_p(n, *p) && mpz_cmp(r, *p) > 0)
+		mpz_add_ui(*p, *p, 2U);
+
+	if (!mpz_divisible_p(n, *p))
+		mpz_set(*p, n);
+}
+
+int main(int ac, char **av)
+{
+	FILE *fp = NULL;
+	char *line = NULL;
+	size_t line_size = 0;
+	ssize_t line_length = -1;
+	mpz_t n, p, q, r;
+
+	mpz_init(n);
+	mpz_init(p);
+	mpz_init(q);
+	mpz_init(r);
+
+	if (ac != 2)
+	{
+		fprintf(stderr, "Error: usage: factors <file>\n");
 		exit(EXIT_FAILURE);
 	}
-	fptr = fopen(argv[1], "r");
-	if (fptr == NULL)
+
+	fp = fopen(av[1], "r");
+	
+	if (!fp)
 	{
-		fprintf(stderr, "Error: can't open file %s\n", argv[1]);
+		fprintf(stderr, "Error: cannot open file %s\n", av[1]);
 		exit(EXIT_FAILURE);
 	}
-	while((line = getline(&buffer, &count, fptr)) != -1)
+
+	while ((line_length = getline(&line, &line_size, fp)) != -1)
 	{
-		factorize(buffer);
+		line[line_length - 1] = '\0';
+		
+		mpz_set_str(n, line, 10);
+
+		factor(&p, n);
+		mpz_tdiv_q(q, n, p);
+
+		mpz_out_str(stdout, 10, n);
+		printf("=");
+		mpz_out_str(stdout, 10, q);
+		printf("*");
+		mpz_out_str(stdout, 10, p);
+		printf("\n");
 	}
-return (0);
+
+	free(line);
+	fclose(fp);
+
+	mpz_clear(n);
+	mpz_clear(p);
+	mpz_clear(q);
+	mpz_clear(r);
+
+	return (0);
 }
--- rsa.c
+++ rsa.c
@@ -1,32 +1,107 @@
-#include "factor.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <gmp.h>
 
-/**
- * main - main function
- *
- * Return: void
- */
-int main(int argc, char *argv[])
+void factor(mpz_t *p, mpz_t n, mpz_t s)
 {
-	FILE *fptr;
-	size_t count;
-	ssize_t line;
-	char *buffer = NULL;
+	mpz_set(*p, s);
 
+	if (mpz_divisible_p(n, s))
+		return;
 
-	if (argc != 2)
+	if (mpz_divisible_ui_p(*p, 2U))
+		mpz_sub_ui(*p, *p, 1U);
+
+	while (!mpz_divisible_p(n, *p) && mpz_cmp_ui(*p, 1U) > 0)
+		mpz_sub_ui(*p, *p, 2U);	
+}
+
+int is_prime(mpz_t op)
+{
+	mpz_t s, r;
+	int yesOrNo = 0;
+	
+	mpz_init(s);
+	mpz_init(r);
+
+	mpz_sqrt(s, op);
+	factor(&r, op, s);
+	
+
+	yesOrNo = (mpz_cmp_ui(r, 1) == 0);
+
+	mpz_clear(s);
+	mpz_clear(r);
+
+	return yesOrNo;
+}
+
+int main(int ac, char **av)
+{
+	FILE *fp = NULL;
+	char *line = NULL;
+	size_t line_size = 0;
+	ssize_t line_length = -1;
+	mpz_t n, p, q, r;
+
+	mpz_init(n);
+	mpz_init(p);
+	mpz_init(q);
+	mpz_init(r);
+
+	if (ac != 2)
 	{
-		fprintf(stderr, "Usage: factor <filename>\n");
+		fprintf(stderr, "Error: usage: factors <file>\n");
 		exit(EXIT_FAILURE);
 	}
-	fptr = fopen(argv[1], "r");
-	if (fptr == NULL)
+
+	fp = fopen(av[1], "r");
+	
+	if (!fp)
 	{
-		fprintf(stderr, "Error: can't open file %s\n", argv[1]);
+		fprintf(stderr, "Error: cannot open file %s\n", av[1]);
 		exit(EXIT_FAILURE);
 	}
-	while((line = getline(&buffer, &count, fptr)) != -1)
+
+	while ((line_length = getline(&line, &line_size, fp)) != -1)
 	{
-		factorize(buffer);
+		line[line_length - 1] = '\0';
+		
+		mpz_set_str(n, line, 10);
+
+		while (1)
+		{
+			while (1)
+			{
+				mpz_sqrt(p, n);
+				factor(&p, n, p);
+				if (is_prime(p))
+					break;
+				mpz_sub_ui(p, p, 2U);
+			}
+			mpz_tdiv_q(q, n, p);
+			if (is_prime(q))
+				break;
+			mpz_sub_ui(p, p, 2U);
+		}
+
+
+		mpz_out_str(stdout, 10, n);
+		printf("=");
+		mpz_out_str(stdout, 10, q);
+		printf("*");
+		mpz_out_str(stdout, 10, p);
+		printf("\n");
 	}
-return (0);
+
+	free(line);
+	fclose(fp);
+
+	mpz_clear(n);
+	mpz_clear(p);
+	mpz_clear(q);
+	mpz_clear(r);
+
+	return (0);
 }
